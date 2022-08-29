01bc57513de0f9bb8c6803e5bebfe1f84767e173
Mohammed 'Mo' Mulazada <thisismoniac@gmail.com>
2022-08-29 14:27:21 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { PrismaClient, Prisma } from "@prisma/client";
+import { ApolloServer, gql } from "apollo-server";
+import { Query, Mutation } from "./resolvers";
+import { typeDefs } from "./schema";
+
+const prisma = new PrismaClient();
+
+export interface Context {
+  prisma: PrismaClient<
+    Prisma.PrismaClientOptions,
+    never,
+    Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined
+  >;
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query,
+    Mutation,
+  },
+  context: {
+    prisma,
+  },
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
