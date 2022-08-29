b901700abc3f0e51ce8effff6bc7587b721a36ea
Mohammed 'Mo' Mulazada <thisismoniac@gmail.com>
2022-08-30 00:03:02 +0200

    add post update and delete mutation

--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -1,26 +1,29 @@
-import { Post } from "@prisma/client";
+import { Post, Prisma } from "@prisma/client";
 import { Context } from "..";
 
-interface PostCreateArgs {
-  title: string;
-  content: string;
+interface PostArgs {
+  post: {
+    title?: string;
+    content?: string;
+  };
 }
 
 interface PostPayloadType {
   userErrors: {
     message: string;
   }[];
-  post: Post | null;
+  post: Post | Prisma.Prisma__PostClient<Post> | null;
 }
 
 export const Mutation = {
   postCreate: async (
     _: any,
-    args: PostCreateArgs,
+    args: PostArgs,
     context: Context
   ): Promise<PostPayloadType> => {
     const { prisma } = context;
-    const { title, content } = args;
+    const { post } = args;
+    const { title, content } = post;
 
     if (!title || !content) {
       return {
@@ -33,10 +36,83 @@ export const Mutation = {
       };
     }
 
-    const post = await prisma.post.create({
+    const newPost = await prisma.post.create({
       data: { title, content, authorId: 1 },
     });
 
-    return { userErrors: [], post };
+    return { userErrors: [], post: newPost };
+  },
+  postUpdate: async (
+    _: any,
+    args: { post: PostArgs["post"]; postId: string },
+    context: Context
+  ): Promise<PostPayloadType> => {
+    const { prisma } = context;
+    const { post, postId } = args;
+    const { title, content } = post;
+
+    if (!title && !content) {
+      return {
+        userErrors: [
+          {
+            message: "Need to have atleast one field",
+          },
+        ],
+        post: null,
+      };
+    }
+
+    const existingPost = await prisma.post.findUnique({
+      where: {
+        id: Number(postId),
+      },
+    });
+
+    if (!existingPost) {
+      return {
+        userErrors: [
+          {
+            message: "Post does not exist",
+          },
+        ],
+        post: null,
+      };
+    }
+
+    return {
+      userErrors: [],
+      post: prisma.post.update({
+        data: { title, content },
+        where: {
+          id: Number(postId),
+        },
+      }),
+    };
+  },
+  postDelete: async (_: any, args: { postId: string }, context: Context) => {
+    const { prisma } = context;
+    const { postId } = args;
+
+    if (!postId) {
+      return {
+        userErrors: [{ message: "You have to provide a post ID." }],
+        post: [],
+      };
+    }
+
+    const deletedPost = await prisma.post.delete({
+      where: {
+        id: Number(postId),
+      },
+    });
+
+    if (!deletedPost) {
+      return {
+        userErrors: [{ message: "Post does not exist" }],
+        post: [],
+      };
+    }
+
+    return { userErrors: [], post: deletedPost };
   },
 };
--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,3 +1,13 @@
+import { Context } from "..";
+
 export const Query = {
-  hello: () => "World",
+  posts: (_: any, __: any, context: Context) => {
+    const { prisma } = context;
+
+    return prisma.post.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  },
 };
